bb8303eaaf35fcc384a97b3ddd3eadc714c2dc08
yuki ohno <jeanadler33@gmail.com>
2021-04-28 16:49:00 +0000

    検証依頼のためにアプリケーションをUP

---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users, only: [:index, :new, :create, :show]
+  resources :relationships, only: [:create, :destroy]
+end
